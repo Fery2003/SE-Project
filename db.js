63b30663d06a4269792396e12a25fd90ee355583
Fery2003 <feras.nizar2003@gmail.com>
2023-05-23 11:47:44 +0300

    imported knex and fixed db.js to match Donia's

--- db.js
+++ db.js
@@ -1,11 +1,23 @@
-const { Pool } = require('pg');
+// import the knex library that will allow us to
+// construct SQL statements
+const knex = require('knex');
 
-const pool = new Pool({
-    user: 'postgres',
-    password: 'YahiaEhab10',
+// define the configuration settings to connect
+// to our local postgres server
+const config = {
+  client: 'pg',
+  connection: {
     host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '1598753',
     database: 'postgres',
-    port: 5432
-});
+  }
+};
 
-module.exports = pool;
+// create the connection with postgres
+const db = knex(config);
+
+// expose the created connection so we can
+// use it in other files to make sql statements
+module.exports = db;
