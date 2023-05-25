7ac4296a66bb3611cb6c690e5daa2c493d7768c9
yahiaehab10 <102036052+yahiaehab10@users.noreply.github.com>
2023-05-25 17:04:10 +0300

    Some fixes Files from the starter Connecting to db

--- connectors/db.js
+++ connectors/db.js
@@ -1,6 +1,7 @@
 // import the knex library that will allow us to
 // construct SQL statements
 const knex = require('knex');
+const { Pool } = require('pg');
 
 // define the configuration settings to connect
 // to our local postgres server
@@ -17,7 +18,9 @@ const config = {
 
 // create the connection with postgres
 const db = knex(config);
+const pool = new Pool(config);
 
 // expose the created connection so we can
 // use it in other files to make sql statements
 module.exports = db;
+module.exports = pool;
---
+++ routes/private/api.js
@@ -0,0 +1,265 @@
+const { isEmpty } = require("lodash");
+const { v4 } = require("uuid");
+const db = require("../../connectors/db");
+const roles = require("../../constants/roles");
+const { getSessionToken } = require('../../utils/session')
+const getUser = async function (req) {
+  const sessionToken = getSessionToken(req);
+  if (!sessionToken) {
+    return res.status(301).redirect("/");
+  }
+  console.log("hi", sessionToken);
+  const user = await db
+    .select("*")
+    .from("se_project.sessions")
+    .where("token", sessionToken)
+    .innerJoin(
+      "se_project.users",
+      "se_project.sessions.userid",
+      "se_project.users.id"
+    )
+    .innerJoin(
+      "se_project.roles",
+      "se_project.users.roleid",
+      "se_project.roles.id"
+    )
+    .first();
+
+  console.log("user =>", user);
+  user.isNormal = user.roleid === roles.user;
+  user.isAdmin = user.roleid === roles.admin;
+  user.isSenior = user.roleid === roles.senior;
+  console.log("user =>", user)
+  return user;
+};
+
+module.exports = function (app) {
+  // example
+  app.get("/users", async function (req, res) {
+    try {
+      const user = await getUser(req);
+      const users = await db.select('*').from("se_project.users")
+
+      return res.status(200).json(users);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not get users");
+    }
+
+  });
+
+  app.get('/', async (req, res) => {
+    try {
+      const { rows } = await db.query('SELECT * FROM se_project.stations');
+      res.json(rows);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  });
+
+  const getUser = async function (req) {
+    const sessionToken = getSessionToken(req);
+    if (!sessionToken) {
+      return res.status(301).redirect('/');
+    }
+
+    const user = await db.select('*')
+      .from('se_project.sessions')
+      .where('token', sessionToken)
+      .innerJoin('se_project.users', 'se_project.sessions.userid', 'se_project.users.id')
+      .innerJoin('se_project.roles', 'se_project.users.roleid', 'se_project.roles.id')
+      .first();
+
+    console.log('user =>', user)
+    user.isStudent = user.roleid === roles.student;
+    user.isAdmin = user.roleid === roles.admin;
+    user.isSenior = user.roleid === roles.senior;
+
+    return user;
+  }
+
+  //User stuff
+
+  // app.get('/dashboard', async (req, res) => {
+  //     try {
+  //         const user = await db.query('SELECT * FROM se_project.users WHERE id = $1', [req.user]);
+  //         res.json(user.rows[0]);
+  //     } catch (error) {
+  //         console.error(error.message);
+  //         res.status(500).send('Server Error!');
+  //     }
+  // });
+
+  // Reset password endpoint
+  app.put('/api/v1/password/reset', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await db.query('UPDATE se_project.users SET password = $1 WHERE email = $2 RETURNING *', [password, email]);
+      res.json(user.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  });
+
+  // Subscriptions endpoint(running but not testing)
+  app.get('/subscriptions/api/v1/zones', async (req, res) => {
+    try {
+      const zones = await db.query('SELECT * FROM se_project.zones');
+      res.json(zones.rows);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  });
+
+  // Create subscription endpoint, same logic of reg
+  app.post('/subscriptions/api/payment/subscription', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+  });
+
+  // Create ticket endpoint, same logic as reg
+  app.post('/tickets/api/payment/tickets', async (req, res) => {
+
+  });
+
+  // Purchase ticket with subscription endpoint
+  app.post('/tickets/api/v1/tickets/purchase/subscription', async (req, res) => {
+    try {
+      const { status, origin, destination, userid, ticketid, tripdate } = req.body;
+      const ticket = await db.query('INSERT INTO se_project.tickets (status, origin, destination, userid, ticketid, tripdate) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [status, origin, destination, userid, ticketid, tripdate]);
+      res.json(ticket.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  });
+
+  // Get ticket price endpoint
+  app.post('/prices/api/v1/tickets/price/:originId', async (req, res) => {
+
+  });
+
+  // Get all rides endpoint
+  // app.get('/rides', async (req, res) => {
+  //     try {
+  //         const rides = await db.query('SELECT * FROM se_project.rides');
+  //         res.json(rides.rows);
+  //     } catch (error) {
+  //         console.error(error.message);
+  //         res.status(500).send('Server Error!');
+  //     }
+  // });
+
+  // Request ticket refund endpoint
+  app.post('/request/refund/api/v1/refund/:ticketId', async (req, res) => {
+
+  });
+
+  // Request senior ticket endpoint
+  app.post('/request/senior/api/v1/senior/request', async (req, res) => {
+
+  });
+
+  // Simulate ride endpoint
+  app.put('/rides/simulate/api/v1/ride/simulate', async (req, res) => {
+
+  });
+
+  //Admin Stuff
+
+  // Reset password endpoint
+  app.put('/api/v1/password/reset', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await db.query('UPDATE se_project.users SET password = $1 WHERE email = $2 RETURNING *', [password, email]);
+      res.json(user.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  });
+
+  // app.get('/dashboard', async (req, res) => {
+  //     try {
+  //         const user = await db.query('SELECT * FROM se_project.users WHERE id = $1', [req.user]);
+  //         res.json(user.rows[0]);
+  //     } catch (error) {
+  //         console.error(error.message);
+  //         res.status(500).send('Server Error!');
+  //     }
+  // });
+
+  // Create new station endpoint
+  app.post('/manage/stations/api/v1/station', async (req, res) => {
+    try {
+      const { stationname, stationtype, stationposition, stationstatus } = req.body;
+      const newStation = await db.query('INSERT INTO se_project.stations (stationname, stationtype, stationposition, stationstatus) VALUES ($1, $2, $3, $4) RETURNING *', [stationname, stationtype, stationposition, stationstatus]);
+      res.json(newStation.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  });
+
+  // Update station endpoint
+  app.put('/manage/stations/api/v1/station/:stationId', async (req, res) => {
+    try {
+      const stationId = req.params
+      const { stationName } = req.body
+      const updateStation = await db.query("UPDATE stations SET stationname = $1 WHERE id = $2 RETURNING *", [stationName, stationId]);
+      res.json(updateStation.rows[0])
+    }
+    catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  });
+
+  // Delete station endpoint
+  app.delete('/manage/stations/api/v1/station/:stationId', async (req, res) => {
+
+  });
+
+  // Create new route endpoint
+  app.post('/manage/routes/api/v1/route', async (req, res) => {
+    try {
+      const { routename, fromStationid, toStationid } = req.body;
+      const newRoute = await db.query('INSERT INTO se_project.routes (routename, fromStationid, toStationid) VALUES ($1, $2, $3) RETURNING *', [routename, fromStationid, toStationid]);
+      res.json(newRoute.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  });
+
+  // Update route endpoint
+  app.put('/manage/routes/api/v1/route/:routeId', async (req, res) => {
+
+  });
+
+  // Delete route endpoint
+  app.delete('/manage/routes/api/v1/route/:routeId', async (req, res) => {
+
+  });
+
+  //Update zone price
+  app.post('/manage/zones/api/v1/zones/:zoneId', async (req, res) => {
+    try {
+      const { newPrice } = req.body;
+      const zoneId = req.params;
+      const updatedPrice = await db.query('UPDATE zones SET price = $1 WHERE id = $2 RETURNING *', [newPrice, zoneId]);
+      res.json(updatedPrice.rows[0]);
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  });
+
+};
