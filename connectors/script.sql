5af344b9c632e219bc1c9effa2c326eb52b132c2
janaelkasabyy <jana.elkasaby@student.giu-uni.de>
2023-05-27 23:42:27 +0300

    api changes (subs payment)

--- connectors/script.sql
+++ connectors/script.sql
@@ -1,4 +1,4 @@
--- Active: 1684831529052@@127.0.0.1@5432@se_project23@se_project
+-- Active: 1684932602990@@127.0.0.1@5432@postgres@public
 -- DROP TABLE IF EXISTS se_project.users;
 
 -- DROP TABLE IF EXISTS roles;
